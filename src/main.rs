c071692b4c23b1e22d082aab8e59aaab0faf8c5e
Angus <angusedmunds>
2024-08-12 12:00:46 +0100

    add bit manipulation fns

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+struct board {
+    board: [[square; 8]; 8]
+}
+
+type bitmask = u16;
+
+struct square { 
+    mask: bitmask
+}
+
+impl square {
+    pub fn new() -> square {
+        square { 
+            mask: 0 
+        }
+    }
+
+    fn flip(&mut self, bit: bitmask) {
+        self.mask ^= bit;
+    }
+
+    fn is_flipped(&self, bit: bitmask) -> bool {
+        self.mask & bit != 0
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut s = square::new();
+
+    println!("{}", s.is_flipped(1));
+    s.flip(1);
+    println!("{}", s.is_flipped(1));
+    println!("{}", s.is_flipped(0));
 }
